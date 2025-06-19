21d6cd1467eb63f5864365685fbb31a92803765a
mfcss <mf@csselectronics.com>
2025-06-19 12:33:51 +0200

    Update

--- modules/cloud_function/outputs.tf
+++ modules/cloud_function/outputs.tf
@@ -1,4 +1,4 @@
 output "function_name" {
   description = "Name of the deployed Cloud Function"
-  value       = google_cloudfunctions_function.mdf_to_parquet_function.name
+  value       = google_cloudfunctions2_function.mdf_to_parquet_function.name
 }
