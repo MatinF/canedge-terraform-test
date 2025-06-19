900e2141b8824d56d3287b51c7c7e107bd3795f7
mfcss <mf@csselectronics.com>
2025-06-19 11:25:28 +0200

    revert

--- modules/buckets/main.tf
+++ modules/buckets/main.tf
@@ -2,10 +2,9 @@
 * Module to create the output bucket for Parquet files
 */
 
-resource "google_beta_storage_bucket" "output_bucket" {
+resource "google_storage_bucket" "output_bucket" {
   name     = "${var.input_bucket_name}-parquet"
   location = var.region
   
   uniform_bucket_level_access = true
-
 }
